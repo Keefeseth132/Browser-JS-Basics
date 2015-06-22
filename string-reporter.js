9d9220625ccefb4a817a51347bbea99c507047ee
Seth Keefer <keeferseth132@gmail.com>
2015-06-22 15:39:33 -0600

    first commit

--- string-reporter.js
+++ string-reporter.js
@@ -1,3 +1,5 @@
 var word = prompt("enter the word");
 
 alert(word + '\n' + word.length + '\n' + word.charAt(3) + '\n' + word.toUpperCase() + '\n' + word.toLowerCase() + '\n' + ' his speech was almost ' + word + ' out of the book ' + '\n' + word.substring(2,4) );
+
+//
